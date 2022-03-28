5218efa0f2db4f549a0ca776f09cc332b61d8a3e
barbara-marina <barbaramarinaufsm@gmail.com>
2022-03-28 02:19:14 -0300

    feature: seats layout

--- src/components/Seats/index.jsx
+++ src/components/Seats/index.jsx
@@ -1,7 +1,7 @@
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Container} from "./style";
+import { Container, ContainerSeats, Seat} from "./style";
 import axios from "axios";
 import Title from "./../Title/index";
 import Footer from "./../Footer/index";
@@ -9,20 +9,62 @@ import Footer from "./../Footer/index";
 export default function Seats() {
     const {idSession} = useParams();
     const [seats, setSeats] = useState({});
+    const [array, setArray] = useState([]);
 
     useEffect(() => {
         const request = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSession}/seats`);
         request.then(response => setSeats(response.data));
         request.catch(response => console.log(response));
     }, [idSession]);
-
+    
     if (!seats.movie) {
         return <div>carregando...</div>
     }
-    
+
+    function teste(id, isAvailable) {
+        if (!isAvailable){
+            alert("Esse assento não está disponível!");
+            return;
+        }
+        if (array.includes(id)){
+            setArray(array.filter((a) => a===id ? false : true));
+        } else {
+            setArray([...array, id]);
+        }
+    } //trocar por early return depois
+
     return (
         <Container>
             <Title text="Selecione o(s) assento(s)" sucess={false}/>
+            <ContainerSeats>
+                {seats.seats.map(({id, name: seat, isAvailable}) => 
+                    <Seat key={id} isAvailable={isAvailable} isSelected={array.includes(id)} onClick={() => teste(id, isAvailable)}>{seat}</Seat>
+                )}
+                <section>
+                    <article>
+                        <Seat key="selected" isAvailable={true} isSelected={true}/>
+                        <p>Selecionado</p>
+                    </article>
+                    <article>
+                        <Seat key="available" isAvailable={true} isSelected={false}/>
+                        <p>Disponível</p>
+                    </article>
+                    <article>
+                        <Seat key="unavailable" isAvailable={false} isSelected={false}/>
+                        <p>Indisponível</p>
+                    </article>
+                </section>               
+            </ContainerSeats>
+
+            <form>
+                <label>Nome do comprador:</label>
+                <input type="text" placeholder="Digite seu nome..."/>
+                <label>CPF do comprador:</label>
+                <input type="text" placeholder="Digite seu CPF..."/>
+                <button>Reservar assentos(s)</button>
+            </form>
+
+
             <Footer movie={seats.movie} day={seats.day} name={seats.name}/>
         </Container>
     );
--- src/components/Seats/style.jsx
+++ src/components/Seats/style.jsx
@@ -4,5 +4,98 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     gap: 11px 30px;
+
+    form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 42px 24px 120px 24px;;
+        gap: 5px;
+        
+        label{
+            width: 100%;
+            font-size: 18px;
+            color: #293845;
+        }
+
+        input {
+            width: 100%;
+            height: 51px;
+            background: #FFFFFF;
+            border: 1px solid #D5D5D5;
+            box-sizing: border-box;
+            border-radius: 3px;
+            padding: 0 18px;
+
+            &::placeholder {
+                font-size: 18px;
+                color: #AFAFAF;
+                font-style: italic;
+            }
+        }
+
+        button {
+            width: 225px;
+            height: 42px;
+            font-size: 18px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            color: #FFFFFF;
+            background: #E8833A;
+            border: none;
+            border-radius: 3px;
+            margin: 57px 0 30px 0;
+        }
+    }
+`;
+export const ContainerSeats = styled.div`
+    width: 100vw;
+    max-width: 371px;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 24px;
+    gap: 18px 7px;
+
+    section {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 16px;
+
+        article {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            p {
+                font-size: 13px;
+                color: #4E5A65;
+                margin-top: 10px;
+            }
+        }
+    }
+`;
+export const Seat = styled.div`
+    width: 26px;
+    height: 26px;
+    color: #000000;
+    background-color: ${props => props.isAvailable ? "#C3CFD9" : "#FBE192"};
+    background-color: ${props => props.isSelected && "#8DD7CF"};
+    border: ${props => props.isAvailable ? "1px solid #808F9D" : "1px solid #F7C52B"};
+    border: ${props => props.isSelected && "1px solid #1AAE9E"};
+    box-sizing: border-box;
+    border-radius: 12px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 11px;
+    text-align: center;
+    cursor: pointer;
 `;
